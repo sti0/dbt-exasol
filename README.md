4cedfc335574bd382781fb8dfc36da6ec2ebb882
Timo S <sti0@users.noreply.github.com>
2022-09-20 20:48:40 +0000

    bump to version 1.2.1

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 data build tool adapter for exasol
 see docs.getdbt.com for more information
 
-# Release 1.0.0
+# Release 1.2.1
 
 - pyexasol HTTP import csv feature implemented. Optimal performance and compatibility with Exasol CSV parsing
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from setuptools import find_packages
+from setuptools import find_namespace_packages
 from distutils.core import setup
 
 package_name = "dbt-exasol"
-package_version = "1.0.3"
+package_version = "1.2.1"
 description = """The exasol adpter plugin for dbt (data build tool)"""
 
 setup(
@@ -11,20 +11,20 @@ setup(
     version=package_version,
     description=description,
     long_description=description,
-    author='Torsten Glunde',
-    author_email='torsten@glunde.de',
-    maintainer='Ilija Kutle',
-    url='https://www.glunde.de',
-    packages=find_packages(),
+    author="Torsten Glunde",
+    author_email="torsten@glunde.de",
+    maintainer="Ilija Kutle",
+    url="https://www.glunde.de",
+    packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     package_data={
-        'dbt': [
-            'include/exasol/dbt_project.yml',
-            'include/exasol/macros/*.sql',
-            'include/exasol/macros/**/*.sql',
+        "dbt": [
+            "include/exasol/dbt_project.yml",
+            "include/exasol/macros/*.sql",
+            "include/exasol/macros/**/*.sql",
         ]
     },
     install_requires=[
-       'dbt-core>={}'.format('0.19.0'),
-       'pyexasol>={}'.format('0.11')
-    ]
+        "dbt-core=={}".format(package_version),
+        "pyexasol>={}".format("0.24"),
+    ],
 )
